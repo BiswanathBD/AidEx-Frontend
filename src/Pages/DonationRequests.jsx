a4ccf13d96cf3d05595e3619fe7e934c015a0ffd
BiswanathBD <biswanath.sarker.bd@gmail.com>
2026-01-09 23:27:21 +0600

    fixed request section responsive issue

--- src/Pages/DonationRequests.jsx
+++ src/Pages/DonationRequests.jsx
@@ -89,7 +89,7 @@ const DonationRequests = () => {
       {loader && <Loader />}
 
       {!loader && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 lg:gap-8 mt-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 lg:gap-8 mt-20">
           {currentRequests.length === 0 ? (
             <div
               className={`col-span-full text-center py-12 ${
